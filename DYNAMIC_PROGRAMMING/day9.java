34eba0fac0bdabfa2dcc1b558e91ab32bcd57f4a
lokeshloki55 <129316191+lokeshloki55@users.noreply.github.com>
2025-02-21 14:36:52 +0530

    Update day9.java

--- DYNAMIC_PROGRAMMING/day9.java
+++ DYNAMIC_PROGRAMMING/day9.java
@@ -15,18 +15,18 @@ public class dp7_ninja {
     {
         if(curr_task!=lasttask)
         {
-            int maxi_curr=calc(day-1, curr_task, arr, dp)+arr[day][curr_task];//taking every element in current day and adding it to previous day recursion and passing lastdaytask=curre_task in current iteration because curr_task is the task performed in the current day
-            maxi=Math.max(maxi,maxi_curr);//oru current day (nth) activity eduthu adha previous day la endha activity kooda add pana maximum varthu nu pakrom and similarly antha day oda ella task kum check panrom side by side maximum value override agite varum.
+            int maxi_curr=calc(day-1, curr_task, arr, dp)+arr[day][curr_task];
+            maxi=Math.max(maxi,maxi_curr);
         }
     }
      return dp[day][lasttask]=maxi;
 }
     public static void main(String[] args) {
         int [][] arr={{10,40,70},{20,50,80},{30,60,90}};
-        int [][] dp=new int[3][4];//3 is number of days and 4 is number of activities that can be performed in a day(3(given activities)+1(no restrictions,used in nth day which is first used in recurrance(see note))
+        int [][] dp=new int[3][4];
         for(int[] eachrow:dp)
         Arrays.fill(eachrow, -1); 
-        int n=3,lasttask=3;//initially last task is taken as 3 because you can take any task without any restrictions initially.
+        int n=3,lasttask=3;
         System.out.println(calc(n-1,lasttask,arr,dp));      
     }    
 }
