610fd8177bed0d895fa35948042f7c4220b68281
lokeshloki55 <129316191+lokeshloki55@users.noreply.github.com>
2025-02-27 15:27:59 +0530

    Create day15.java

---
+++ DYNAMIC_PROGRAMMING/day15.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class dp10_minpath_top_bottom {
+    static int countwaysss(int i,int j,int[][] arr,int[][] dp)
+{   
+    if(i==0&&j==0)
+    return arr[0][0];  //return the current element for addition and no further recursion has to be take place
+    if(i<0 || j<0)
+    return Integer.MAX_VALUE;// if index is beyond the range return maximum value so that path is neglected by min().
+    if (dp[i][j]!=-1)
+    return dp[i][j];
+    int up=arr[i][j]+countwaysss(i-1, j, arr,dp);    //take the current element to sum and got to top element and do the same in recursion
+    int left=arr[i][j]+countwaysss(i, j-1, arr,dp);//take the current element to sum and got to left element and do the same in recursion
+    return dp[i][j]= Math.min(up,left);
+
+}
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int [][] arr={{5,9,6},{11,5,2}};
+        int n=arr.length;
+        int m=arr[0].length;
+        int [][] dp=new int[n][m];
+        for(int[] eachrow:dp)
+        Arrays.fill(eachrow, -1);   //array.fill works for only 1D array so we use for loop and take each row in 2d array and fill it with -1.
+        System.out.println(countwaysss(n-1,m-1,arr,dp));
+    }}
